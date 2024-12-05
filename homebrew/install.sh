a6032784061ff765055190abc3680966bc6f8f13
Ranger Chan <rangerc@dcius.com>
2024-12-05 14:09:19 -0600

    homebrew

--- homebrew/install.sh
+++ homebrew/install.sh
@@ -2,6 +2,8 @@
 
 set -e
 
+CUR_DIR="$(cd "$( dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
+
 [[ "$(uname)" == "Darwin" ]] || { echo "Support MacOS only."; exit 0; }
 
 if command -v brew &> /dev/null; then
@@ -10,39 +12,25 @@ else
     /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 fi
 
-formulae=(
-    mas
-    node
-    pnpm
-    php
-    composer
-    python
-    wget
-    colordiff
-    git-delta
-    git-extras
-)
+read_file_to_array() {
+    local file="$1"
+    local array_name="$2"
 
+    eval "$array_name=()"
+
+    while IFS= read -r line; do
+        eval "$array_name+=('$line')"
+    done < "$file"
+}
+
+formulae=()
+casks=()
+
+read_file_to_array "$CUR_DIR/formulae" formulae
+read_file_to_array "$CUR_DIR/casks" casks
+
+# Install packages and casks
 brew install "${formulae[@]}"
-
-casks=(
-    google-chrome
-    microsoft-edge
-    firefox
-    caffeine
-    the-unarchiver
-    microsoft-teams
-    diffmerge
-    visual-studio-code
-    phpstorm
-    sequel-pro
-    azure-data-studio
-    iterm2
-    mark-text
-    orbstack
-    notion
-)
-
-echo brew install --cask "${casks[@]}"
+brew install --cask "${casks[@]}"
 
 brew cleanup
